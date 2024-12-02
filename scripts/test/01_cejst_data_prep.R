60e7d051ed7deab1972145c6924f6b214176be5e
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-12-02 16:12:04 -0700

    completed cejst social data prep need to update the download_data function

--- scripts/test/01_cejst_data_prep.R
+++ scripts/test/01_cejst_data_prep.R
@@ -4,6 +4,9 @@ library(terra)
 library(tigris)
 library(stringr)
 library(RCurl)
+library(stars)
+library(spdep)
+library(gstat)
 # Set timeout to 10 minutes
 options(timeout=6000)
 
@@ -41,5 +44,91 @@ download_data <- function(url, file_name) {
 url <- "https://static-data-screeningtool.geoplatform.gov/data-versions/1.0/data/score/downloadable/1.0-shapefile-codebook.zip"
 file_name <- ""
 
+# NOTE TO SELF: Need to fix the download data function
+download_data(url, file_name)
+unzip(here::here("data/original/usa.zip"), exdir = here::here("data/original/cejst/"))
+
+# read in the cejst shapefile
+cejst <- read_sf(here::here("data/original/cejst/usa.shp"))
+
+# Filter for Idaho
+filt_cejst <- cejst %>%
+  filter(SF == "Idaho")
+
+## Select variables: less high school, housing burden, energy burden, and PM2.5
+cejst_vars <- filt_cejst %>% 
+  dplyr::select(geometry, HSEF, HBF_PFS, EBF_PFS, PM25F_PFS)
+
+## check for validity, remove empty geometries, and reproject 
+if (!all(st_is_valid(cejst_vars)))
+  cejst_vars <- st_make_valid(cejst_vars)
+
+cejst_vars <- cejst_vars %>%
+  filter(!st_is_empty(.))
+
+projection <- "epsg:5070"
+
+cejst_vars_proj <- cejst_vars %>%
+  st_transform(projection)
+
+## Create a template raster for the shapefiles
+XMIN <- ext(cejst_vars_proj)$xmin
+XMAX <- ext(cejst_vars_proj)$xmax
+YMIN <- ext(cejst_vars_proj)$ymin
+YMAX <- ext(cejst_vars_proj)$ymax
+aspectRatio <- (YMAX-YMIN)/(XMAX-XMIN)
+cellSize <- 3000
+NCOLS <- as.integer((XMAX-XMIN)/cellSize)
+NROWS <- as.integer(NCOLS * aspectRatio)
+templateRas <- rast(ncol=NCOLS, nrow=NROWS, 
+                    xmin=XMIN, xmax=XMAX, ymin=YMIN, ymax=YMAX,
+                    vals=1, crs=crs(cejst_vars_proj))
+
+grd <- st_as_stars(templateRas)
+
+# function to rasterize variable, make points, make predictions
+# raster_grid is like a reference raster
+idw_preds <- function(data_proj, ref_raster, lay, empty_grid){
+  var.rst <- rasterize(data_proj, ref_raster, field = lay, fun = "mean")
+  var.pt <- as.points(var.rst) %>%
+    st_as_sf(.)
+  var.pred <- idw(var.pt[[1]]~1, var.pt, empty_grid)
+  var.pred.rst <- rasterize(st_as_sf(var.pred), ref_raster, field = "var1.pred")
+  names(var.pred.rst) <- paste0(lay, ".pred")
+  return(c(orig.rst = var.rst, pred.rst = var.pred.rst))
+}
+
+# Less high school
+lesshs.preds <- idw_preds(cejst_vars_proj, templateRas, "HSEF", grd)
+plot(lesshs.preds$orig.rst)
+plot(lesshs.preds$pred.rst)
+lesshs_crop <- crop(lesshs.preds$pred.rst, cejst_vars_proj, mask = TRUE)
+plot(lesshs_crop)
+writeRaster(x = lesshs_crop, here::here(paste0("data/processed/cejst_lesshs_id_3km_pred_crop_", 
+                                                          Sys.Date(), ".tif")), overwrite = TRUE)
+
+# Housing burden 
+hsburd.preds <- idw_preds(cejst_vars_proj, templateRas, "HBF_PFS", grd)
+plot(hsburd.preds$orig.rst)
+houseburd_crop <- crop(hsburd.preds$pred.rst, cejst_vars_proj, mask = TRUE)
+plot(houseburd_crop)
+writeRaster(houseburd_crop, here::here(paste0("data/processed/cejst_hsbrd_id_3km_pred_crop_", 
+                                           Sys.Date(), ".tif")), overwrite = TRUE)
+
+# Energy burden 
+engburd.preds <- idw_preds(cejst_vars_proj, templateRas, "EBF_PFS", grd)
+plot(engburd.preds$orig.rst)
+engburd_crop <- crop(engburd.preds$pred.rst, cejst_vars_proj, mask = TRUE)
+plot(engburd_crop)
+writeRaster(engburd_crop, here::here(paste0("data/processed/cejst_engbrd_id_3km_pred_crop_", 
+                                              Sys.Date(), ".tif")), overwrite = TRUE)
+
+# PM2.5 exposure 
+pm25.preds <- idw_preds(cejst_vars_proj, templateRas, "PM25F_PFS", grd)
+plot(pm25.preds$orig.rst)
+pm25_crop <- crop(pm25.preds$pred.rst, cejst_vars_proj, mask = TRUE)
+plot(pm25_crop)
+writeRaster(pm25_crop, here::here(paste0("data/processed/cejst_pm25_id_3km_pred_crop_", 
+                                            Sys.Date(), ".tif")), overwrite = TRUE)
 
 
