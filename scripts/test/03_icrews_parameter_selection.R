d828cd98f792c092300a4cf6aa606f487c942172
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-01-07 16:34:05 -0700

    added code to work through generalized FCM

--- scripts/test/03_icrews_parameter_selection.R
+++ scripts/test/03_icrews_parameter_selection.R
@@ -45,3 +45,54 @@ ggplot(FCMvalues) +
   geom_text(aes(x = k, y = m, label = round(XieBeni.index,2)), size = 2.5)+
   scale_fill_viridis() +
   coord_fixed(ratio=2)
+
+#----Use a generalized fuzzy c-means to determine the value for m and beta
+# k = 3
+GFCMvalues_k3 <- select_parameters.mc(algo = "GFCM", data = dataset, seed = 6891,
+                                      k = 3, m = seq(1.1,2,0.1), beta = seq(0.1,0.9,0.1),
+                                      spconsist = FALSE, verbose = TRUE, init = "kpp",
+                                      indices = c("XieBeni.index", "Explained.inertia",
+                                                  "Negentropy.index", "Silhouette.index"))  
+
+write_csv(GFCMvalues_k3, here::here(paste0("outputs/parameter_selection/gfcm_param_indices_k3_",
+                                          Sys.Date(), ".csv")), append = FALSE)
+
+
+# plotting the silhouette index
+ggplot(GFCMvalues_k3) + 
+  geom_raster(aes(x = m, y = beta, fill = Silhouette.index)) + 
+  geom_text(aes(x = m, y = beta, label = round(Silhouette.index,2)), size = 2)+
+  scale_fill_viridis() +
+  coord_fixed(ratio=1)
+
+# plotting the Xie Beni
+ggplot(GFCMvalues_k3) + 
+  geom_raster(aes(x = m, y = beta, fill = XieBeni.index)) + 
+  geom_text(aes(x = m, y = beta, label = round(XieBeni.index, 2)), size = 2)+
+  scale_fill_viridis() +
+  coord_fixed(ratio=1)
+
+# k = 8
+GFCMvalues_k8 <- select_parameters.mc(algo = "GFCM", data = dataset, seed = 6891,
+                                      k = 8, m = seq(1.1,2,0.1), beta = seq(0.1,0.9,0.1),
+                                      spconsist = FALSE, verbose = TRUE, init = "kpp",
+                                      indices = c("XieBeni.index", "Explained.inertia",
+                                                  "Negentropy.index", "Silhouette.index"))  
+
+write_csv(GFCMvalues_k8, here::here(paste0("outputs/parameter_selection/gfcm_param_indices_k8_",
+                                           Sys.Date(), ".csv")), append = FALSE)
+
+
+# plotting the silhouette index
+ggplot(GFCMvalues_k8) + 
+  geom_raster(aes(x = m, y = beta, fill = Silhouette.index)) + 
+  geom_text(aes(x = m, y = beta, label = round(Silhouette.index,2)), size = 2)+
+  scale_fill_viridis() +
+  coord_fixed(ratio=1)
+
+# plotting the Xie Beni
+ggplot(GFCMvalues_k8) + 
+  geom_raster(aes(x = m, y = beta, fill = XieBeni.index)) + 
+  geom_text(aes(x = m, y = beta, label = round(XieBeni.index, 2)), size = 2)+
+  scale_fill_viridis() +
+  coord_fixed(ratio=1)
