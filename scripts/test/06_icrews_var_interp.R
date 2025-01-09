98e436ba89a8d42c2eebaff1c0cded4d4d32051e
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-01-09 12:51:36 -0700

    updated to finish making the IQR and SD "barcode" plots for k=8 clusters

--- scripts/test/06_icrews_var_interp.R
+++ scripts/test/06_icrews_var_interp.R
@@ -17,51 +17,37 @@ library(forcats)
 rst_sc <- rast(here::here("data/processed/rast_stack_all_attributes_scaled_2025-01-07.tif"))
 rst <- rast(here::here("data/processed/rast_stack_all_attributes_2025-01-07.tif"))
 
-gfcm_k3_result <- rast(here::here("outputs/models/gfcm_result_k3_2025-01-08.tif"))
+#gfcm_k3_result <- rast(here::here("outputs/models/gfcm_result_k3_2025-01-08.tif"))
 sgfcm_k8_result <- rast(here::here("outputs/models/sgfcm_result_k8_2025-01-08.tif"))
 
-plot(gfcm_k3_result)
+#plot(gfcm_k3_result)
 plot(sgfcm_k8_result)
 
-gfcm_result_k3_mod <- readRDS(here::here("outputs/models/gfcm_mod_k3_2025-01-08.rds"))
+#gfcm_result_k3_mod <- readRDS(here::here("outputs/models/gfcm_mod_k3_2025-01-08.rds"))
 sgfcm_result_k8_mod <- readRDS(here::here("outputs/models/sgfcm_mod_k8_2025-01-08.rds"))
 
 
 # Variable interp plot
 #data <- as.data.frame(rst_sc)
 data <- as.data.frame(scale(rst))
-data$groups_k3 <- gfcm_k3_result$Groups
-data$groups_k8 <- sgfcm_k8_result$Groups
+#data$groups_k3 <- gfcm_result_k3_mod$Groups
+data$groups_k8 <- sgfcm_result_k8_mod$Groups
 
-data$groups_k3 <- gsub('V', 'A', data$groups_k3)
+#data$groups_k3 <- gsub('V', 'A', data$groups_k3)
 data$groups_k8 <- gsub('V', 'A', data$groups_k8)
 
-k3_long_df <- data %>%
-  dplyr::select(-groups_k8) %>%
-  pivot_longer(!groups_k6, names_to = "var_name")
-
-k3_means_long <- data %>%
-  group_by(groups_k3) %>%
-  summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
-  pivot_longer(!groups_k3, names_to = "var_name", values_to = "mean")
-
-k3_sd_long <- data %>%
-  group_by(groups_k3) %>%
-  summarise(across(where(is.numeric), ~ sd(.x, na.rm = TRUE))) %>% 
-  pivot_longer(!groups_k3, names_to = "var_name", values_to = "sd")
-
-k3_med_long <- data %>%
-  group_by(groups_k3) %>%
-  summarise(across(where(is.numeric), ~ median(.x, na.rm = TRUE))) %>% 
-  pivot_longer(!groups_k3, names_to = "var_name", values_to = "median")
-
-k3_long <- left_join(k3_means_long, k3_sd_long)
-k3_long <- left_join(k3_long, k3_med_long)
+k8_long_df <- data %>%
+  pivot_longer(!groups_k8, names_to = "var_name")
 
 k8_means_long <- data %>%
   group_by(groups_k8) %>%
   summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
   pivot_longer(!groups_k8, names_to = "var_name", values_to = "mean")
+
+k8_med_long <- data %>%
+  group_by(groups_k8) %>%
+  summarise(across(where(is.numeric), ~ median(.x, na.rm = TRUE))) %>% 
+  pivot_longer(!groups_k8, names_to = "var_name", values_to = "median")
 
 k8_sd_long <- data %>%
   group_by(groups_k8) %>%
@@ -69,75 +55,51 @@ k8_sd_long <- data %>%
   pivot_longer(!groups_k8, names_to = "var_name", values_to = "sd")
 
 k8_long <- left_join(k8_means_long, k8_sd_long)
+k8_long <- left_join(k8_long, k8_med_long)
 
 # reorder the variables
-k3_long_reorder <- k3_long %>% 
+k8_long_reorder <- k8_long %>% 
   mutate(var_name = fct_relevel(var_name, 
-                                "treecov", "forprod", "tempseas", 
-                                "precseas", "rough", "whp", 
-                                "forgain", "distcrit", "distwild",
-                                "pm25", "fedrich", 
-                                "treeage", "pct_forpay", "pct_delmill",
-                                "netmig", "comm_cap", "aip", 
-                                "travtime", "hsbrd", "engbrd", "lesshs"))
+                                "comm_cap", "eng_burd", "ag_loss", "net_mig", 
+                                "dist_crithab", "dist_wilderness",
+                                "burn_prob", "ann_max_swe",
+                                "dist_powerplant", "dist_windturb", "dist_solar"))
 
-k3_long_reorder <- k3_long_reorder %>%
-  mutate(sets = case_when(var_name == "treecov" | var_name == "forprod"  ~ "social",
-                            var_name == "distwild" | var_name == "distcrit"  ~ "ecological",
-                            var_name == "fedrich" | var_name == "pm25"  ~ "technological"))
+k8_long_reorder <- k8_long_reorder %>%
+  mutate(sets = case_when(var_name == "comm_cap" | var_name == "eng_burd" | var_name == "ag_loss" | var_name == "net_mig"  | var_name == "dist_crithab" | var_name == "dist_wilderness" ~ "social",
+                            var_name == "burn_prob" | var_name == "ann_max_swe"  ~ "ecological",
+                            var_name == "dist_powerplant" | var_name == "dist_windturb" | var_name == "dist_solar"  ~ "technological"))
 
 
-k3_var_interp <- ggplot(k3_long_reorder, aes(x = var_name, y = mean, fill = sets)) +
+k8_var_interp <- ggplot(k8_long_reorder, aes(x = var_name, y = mean, fill = sets)) +
   geom_col() +
   geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
                 position=position_dodge(.9)) +
-  #scale_fill_brewer(palette = "Set2") +
+  scale_fill_met_d("Kandinsky") +
   coord_flip() +
-  facet_wrap(~groups_k3) +
-  theme(text = element_text(size = 20),
+  facet_wrap(~groups_k8, ncol = 4) +
+  theme(text = element_text(size = 16),
         legend.position = "right", 
         axis.title.y = element_blank()) 
 
-k3_var_interp
+k8_var_interp
 #ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_", Sys.Date(), ".png"), 
 #       plot = k6_var_interp, width = 12, height = 8, dpi = 300) 
 
-k6_var_interp_sd <- ggplot(k6_long_reorder, aes(x = var_name, y = sd, fill = ostrom)) +
-  geom_col() +
-  #scale_fill_brewer(palette = "Set2") +
-  coord_flip() +
-  facet_wrap(~groups_k6) +
-  theme(text = element_text(size = 20),
-        legend.position = "right", 
-        axis.title.y = element_blank()) 
-
-k6_var_interp_sd
-ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_sd_", Sys.Date(), ".png"), 
-       plot = k6_var_interp_sd, width = 12, height = 8, dpi = 300) 
-
-k6_var_interp_med <- ggplot(k6_long_reorder, aes(x = var_name, y = median, fill = ostrom)) +
+k8_var_interp_med <- ggplot(k8_long_reorder, aes(x = var_name, y = median, fill = sets)) +
   geom_col() +
   geom_errorbar(aes(ymin=median-sd, ymax=median+sd), width=.2,
                 position=position_dodge(.9)) +
-  #scale_fill_brewer(palette = "Set2") +
+  scale_fill_met_d("Kandinsky") +
   coord_flip() +
-  facet_wrap(~groups_k6) +
+  facet_wrap(~groups_k8, ncol = 4) +
   theme(text = element_text(size = 20),
         legend.position = "right", 
         axis.title.y = element_blank()) 
 
-k6_var_interp_med
-ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_med_", Sys.Date(), ".png"), 
-       plot = k6_var_interp_med, width = 12, height = 8, dpi = 300)
-
-ggplot(k8_long, aes(x = var_name, y = mean, fill = groups_k8)) +
-  geom_col() +
-  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
-                position=position_dodge(.9)) +
-  coord_flip() +
-  facet_wrap(~groups_k8, ncol = 4) +
-  theme(legend.position = "none", 
-        axis.title.y = element_blank()) 
+k8_var_interp_med
+#ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_med_", Sys.Date(), ".png"), 
+#       plot = k8_var_interp_med, width = 12, height = 8, dpi = 300)
 
 # Create bar plots of the variables where IQR != 0
 check_iqr_overlap <- function(x) {
@@ -149,99 +111,135 @@ check_iqr_overlap <- function(x) {
   return(lower <= 0 & upper >= 0)
 }
 
-overlap <- k6_long_df %>% 
-  group_by(groups_k6, var_name) %>% 
+overlap <- k8_long_df %>% 
+  group_by(groups_k8, var_name) %>% 
   summarise(overlap = check_iqr_overlap(value), .groups="drop")
 
 
-k6_long_join <- k6_long_df %>% 
+k8_long_join <- k8_long_df %>% 
   left_join(overlap) %>% 
   filter(overlap == FALSE)
 
 # reorder the variables
-k6_long_overlap_reorder <- k6_long_join %>% 
+ k8_long_overlap_reorder <- k8_long_join %>% 
   mutate(var_name = fct_relevel(var_name, 
-                                "treecov", "forprod", "tempseas", 
-                                "precseas", "rough", "whp", 
-                                "forgain", 
-                                #"distcrit", 
-                                "distwild",
-                                "pm25", "fedrich", 
-                                "treeage", "pct_forpay", "pct_delmill",
-                                "netmig", "comm_cap", "aip",
-                                "travtime", "hsbrd", "engbrd"
-                                #"lesshs"
+                                "comm_cap", "eng_burd", "ag_loss", "net_mig", 
+                                "dist_crithab", "dist_wilderness",
+                                "burn_prob", "ann_max_swe",
+                                "dist_powerplant", "dist_windturb", "dist_solar"
   ))
 
-k6_long_overlap_reorder <- k6_long_overlap_reorder %>% # need to remove dist to critical habitat and less hs
-  mutate(ostrom = case_when(var_name == "treecov" | var_name == "forprod" | var_name == "tempseas" | var_name == "precseas" | var_name == "rough" | var_name == "whp" ~ "resource system",
-                            var_name == "distwild" | var_name == "forgain" ~ "resource unit",
-                            var_name == "fedrich" | var_name == "pm25" | var_name == "treeage" | var_name == "pct_forpay" | var_name == "pct_delmill" | var_name == "netmig" | var_name == "comm_cap" | var_name == "aip" | var_name == "travtime" | var_name == "hsbrd" | var_name == "engbrd" | var_name == "lesshs" ~ "users"))
+k8_long_overlap_reorder <- k8_long_overlap_reorder %>% # need to remove dist to critical habitat and less hs
+  mutate(sets = case_when(var_name == "comm_cap" | var_name == "eng_burd" | var_name == "ag_loss" | var_name == "net_mig"  | var_name == "dist_crithab" | var_name == "dist_wilderness" ~ "social",
+                          var_name == "burn_prob" | var_name == "ann_max_swe"  ~ "ecological",
+                          var_name == "dist_powerplant" | var_name == "dist_windturb" | var_name == "dist_solar"  ~ "technological"))
 
+# replace var_name with more easily interpreted names 
+k8_long_overlap_reorder_newnames <- k8_long_overlap_reorder %>%
+  mutate(new_var_name = case_when(var_name == "burn_prob" ~ "burn probability",
+                                  var_name == "dist_wilderness" ~ "dist. to wilderness area",
+                                  var_name == "dist_crithab" ~ "dist. to critical habitat",
+                                  var_name == "net_mig" ~ "net migration 2022", 
+                                  var_name == "comm_cap" ~ "community capital",
+                                  var_name == "ag_loss" ~ "expected ag. loss rate",
+                                  var_name == "eng_burd" ~ "energy burden",
+                                  var_name == "ann_max_swe" ~ "annual max swe",
+                                  var_name == "dist_powerplant" ~ "dist. to power plant",
+                                  var_name == "dist_windturb" ~ "dist. to wind turbines",
+                                  var_name == "dist_solar" ~ "dist. to solar fields"))
 
-k6_iqr_no_overlap <- ggplot(data=k6_long_overlap_reorder, mapping = aes(x=var_name, y=value, fill=ostrom)) +
+# reorder the variables
+k8_long_overlap_reorder_newnames <- k8_long_overlap_reorder_newnames %>% 
+  mutate(new_var_name = fct_relevel(new_var_name, 
+                                    "community capital", 
+                                    "energy burden", 
+                                    "expected ag. loss rate", 
+                                    "net migration 2022", 
+                                    "dist. to critical habitat",
+                                    "dist. to wilderness area",
+                                    "burn probability", 
+                                    "annual max swe", 
+                                    "dist. to power plant", 
+                                    "dist. to wind turbines", 
+                                    "dist. to solar fields"
+  ))
+
+k8_iqr_no_overlap <- ggplot(data=k8_long_overlap_reorder_newnames, mapping = aes(x=new_var_name, y=value, fill=sets)) +
   geom_boxplot(outliers = FALSE, coef=0) +
   geom_hline(yintercept = 0, linetype=2) +
-  #scale_fill_manual(values=c15)+
+  scale_fill_met_d("Kandinsky")+
   coord_flip()+
   theme_bw()+
-  facet_wrap(vars(groups_k6))
+  facet_wrap(vars(groups_k8), ncol = 4)
 
-k6_iqr_no_overlap
-ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_iqr_no_overlap_", Sys.Date(), ".png"), 
-       plot = k6_iqr_no_overlap, width = 12, height = 8, dpi = 300)
+k8_iqr_no_overlap
+ggsave(here::here(paste0("outputs/figures/sgfcm_k8_var_interp_iqr_no_overlap_", Sys.Date(), ".png")), 
+       plot = k8_iqr_no_overlap, width = 12, height = 8, dpi = 300)
 
 # Create bar plots of the standard deviation for the values that do overlap with 0
-k6_long_join_overlap_true <- k6_long_df %>% 
+k8_long_join_overlap_true <- k8_long_df %>% 
   left_join(overlap) %>% 
   filter(overlap == TRUE)
 
-k6_sd_overlap <- k6_long_join_overlap_true %>% 
-  group_by(groups_k6, var_name) %>% 
+k8_sd_overlap <- k8_long_join_overlap_true %>% 
+  group_by(groups_k8, var_name) %>% 
   summarise(sd = sd(value), .groups="drop")
 
 # reorder the variables
-k6_sd_overlap_reorder <- k6_sd_overlap %>% 
+k8_sd_long_overlap_reorder <- k8_sd_overlap %>% 
   mutate(var_name = fct_relevel(var_name, 
-                                #"treecov", 
-                                "forprod", "tempseas", 
-                                "precseas", "rough", "whp", 
-                                "forgain", 
-                                "distcrit", 
-                                "distwild",
-                                #"pm25", 
-                                "fedrich", 
-                                "treeage", "pct_forpay", 
-                                #"pct_delmill",
-                                "netmig", "comm_cap", "aip", 
-                                "travtime", "hsbrd", "engbrd", "lesshs"))
+                                "comm_cap", "eng_burd", "ag_loss", "net_mig", 
+                                "dist_crithab", "dist_wilderness",
+                                "burn_prob", "ann_max_swe",
+                                "dist_powerplant", "dist_windturb", "dist_solar"
+  ))
 
-k6_sd_long_overlap_reorder <- k6_sd_overlap_reorder %>% # need to remove treecover, pm25, and pct_delmill
-  mutate(ostrom = case_when(var_name == "forprod" | var_name == "tempseas" | var_name == "precseas" | var_name == "rough" | var_name == "whp" ~ "resource system",
-                            var_name == "distwild" | var_name == "distcrit" |  var_name == "forgain" ~ "resource unit",
-                            var_name == "fedrich" | var_name == "treeage" | var_name == "pct_forpay" | var_name == "netmig" | var_name == "comm_cap" | var_name == "aip" | var_name == "travtime" | var_name == "hsbrd" | var_name == "engbrd" | var_name == "lesshs" ~ "users"))
+k8_sd_long_overlap_reorder <- k8_sd_long_overlap_reorder %>% # need to remove dist to critical habitat and less hs
+  mutate(sets = case_when(var_name == "comm_cap" | var_name == "eng_burd" | var_name == "ag_loss" | var_name == "net_mig"  | var_name == "dist_crithab" | var_name == "dist_wilderness" ~ "social",
+                          var_name == "burn_prob" | var_name == "ann_max_swe"  ~ "ecological",
+                          var_name == "dist_powerplant" | var_name == "dist_windturb" | var_name == "dist_solar"  ~ "technological"))
 
+# replace var_name with more easily interpreted names 
+k8_sd_long_overlap_reorder_newnames <- k8_sd_long_overlap_reorder %>%
+  mutate(new_var_name = case_when(var_name == "burn_prob" ~ "burn probability",
+                                  var_name == "dist_wilderness" ~ "dist. to wilderness area",
+                                  var_name == "dist_crithab" ~ "dist. to critical habitat",
+                                  var_name == "net_mig" ~ "net migration 2022", 
+                                  var_name == "comm_cap" ~ "community capital",
+                                  var_name == "ag_loss" ~ "expected ag. loss rate",
+                                  var_name == "eng_burd" ~ "energy burden",
+                                  var_name == "ann_max_swe" ~ "annual max swe",
+                                  var_name == "dist_powerplant" ~ "dist. to power plant",
+                                  var_name == "dist_windturb" ~ "dist. to wind turbines",
+                                  var_name == "dist_solar" ~ "dist. to solar fields"))
 
-k6_sd_overlap <- ggplot(k6_sd_long_overlap_reorder, aes(x = var_name, y = sd, fill = ostrom)) +
+# reorder the variables
+k8_sd_long_overlap_reorder_newnames <- k8_sd_long_overlap_reorder_newnames %>% 
+  mutate(new_var_name = fct_relevel(new_var_name, 
+                                    "community capital", 
+                                    "energy burden", 
+                                    "expected ag. loss rate", 
+                                    "net migration 2022", 
+                                    "dist. to critical habitat",
+                                    "dist. to wilderness area",
+                                    "burn probability", 
+                                    "annual max swe", 
+                                    "dist. to power plant", 
+                                    "dist. to wind turbines", 
+                                    "dist. to solar fields"
+  ))
+
+k8_sd_overlap <- ggplot(k8_sd_long_overlap_reorder_newnames, aes(x = new_var_name, y = sd, fill = sets)) +
   geom_col() +
-  #scale_fill_brewer(palette = "Set2") +
-  coord_flip() +
-  facet_wrap(~groups_k6) +
-  theme(text = element_text(size = 20),
-        legend.position = "right", 
-        axis.title.y = element_blank()) 
-
-k6_sd_overlap
-ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_sd_overlap_", Sys.Date(), ".png"), 
-       plot = k6_sd_overlap, width = 12, height = 8, dpi = 300)
-
-
-ggplot(data=k6_long_overlap_reorder, mapping = aes(x=var_name, y=sd, fill=ostrom)) +
-  geom_boxplot(outliers = FALSE, coef=0) +
   geom_hline(yintercept = 0, linetype=2) +
-  #scale_fill_manual(values=c15)+
+  scale_fill_met_d("Kandinsky")+
   coord_flip()+
   theme_bw()+
-  facet_wrap(vars(groups_k6))
+  facet_wrap(vars(groups_k8), ncol = 4)
+
+k8_sd_overlap
+ggsave(here::here(paste0("outputs/figures/sgfcm_k8_var_interp_sd_overlap_", Sys.Date(), ".png")), 
+       plot = k8_sd_overlap, width = 12, height = 8, dpi = 300)
+
 
 
