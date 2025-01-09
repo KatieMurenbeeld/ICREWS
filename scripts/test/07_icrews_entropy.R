9f326268d7f0ba19e99b225e895cb706db950875
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-01-09 12:52:26 -0700

    updated to calculate entropy for the ICREWS k = 8 SGFCM clusters. Added code to set projection of the resulting raster

--- scripts/test/07_icrews_entropy.R
+++ scripts/test/07_icrews_entropy.R
@@ -12,11 +12,14 @@ library(spdep)
 library(classInt)
 
 #----Load the data----
-#sgfcm_all_result_k6_mod <- readRDS(here::here("outputs/SGFCM_all_attr_k6_2024-10-15.rds"))
-#sgfcm_all_result_k8_mod <- readRDS(here::here("outputs/SGFCM_all_attr_k8_2024-10-15.rds"))
+#gfcm_result_k3_mod <- readRDS(here::here("outputs/SGFCM_all_attr_k6_2024-10-15.rds"))
+sgfcm_result_k8_mod <- readRDS(here::here("outputs/models/sgfcm_mod_k8_2025-01-08.rds"))
+
+## set the projection
+projection <- "epsg:5070"
+
 # need to rerun the cluster to get model results
-rst_sc <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_scaled_2024-10-08.tif")
-rst <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-08.tif")
+rst_sc <- rast(here::here("data/processed/rast_stack_all_attributes_scaled_2025-01-07.tif"))
 
 # Format for use in geocmeans
 dataset <- lapply(names(rst_sc), function(n){
@@ -29,37 +32,17 @@ names(dataset) <- names(rst_sc)
 # FCM seed = 1234, Silhouette index = 0.48, k = 6, m = 1.9, window =  7x7 (w2), alpha = 0.6, beta = 0.4
 # FCM seed = 1234, Silhouette index = 0.45, k = 8, m = 1.9, window =  3x3 (w1), alpha = 0.5, beta = 0.4
 
-w1 <- matrix(1, nrow = 3, ncol = 3)
-w2 <- matrix(1, nrow = 7, ncol = 7)
+w <- matrix(1, nrow = 5, ncol = 5)
 
-SGFCM_all_result_k6 <- SGFCMeans(dataset, k = 6, m = 1.9, standardize = FALSE,
-                                 lag_method = "mean",
-                                 window = w2, alpha = 0.6, beta = 0.4,
-                                 seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
-
-
-SGFCM_all_result_k8 <- SGFCMeans(dataset, k = 8, m = 1.9, standardize = FALSE,
-                                 lag_method = "mean",
-                                 window = w1, alpha = 0.5, beta = 0.4,
-                                 seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
+SGFCM_result_k8 <- SGFCMeans(dataset, k = 8, m = 1.9, standardize = FALSE,
+                             lag_method = "mean",
+                             window = w, alpha = 1.6, beta = 0.2,
+                             seed = 456, tol = 0.001, verbose = TRUE, init = "kpp")
 
 
 # Calculate Entropy
-## k = 6
-arch_all_rst <- rast(SGFCM_all_result_k6$rasters)
-arch_all_rst_conus_belong <- subset(arch_all_rst, 1:6)
-
-arch_all_rst_conus_df <- as.data.frame(arch_all_rst, xy = TRUE)
-
-all_ent_conus <- calcUncertaintyIndex(as.matrix(as.data.frame(arch_all_rst_conus_belong)))
-
-all_ent_conus <- cbind(arch_all_rst_conus_df, all_ent_conus)
-all_ent_rst_conus <- rast(all_ent_conus)
-
-writeRaster(all_ent_rst_conus, here::here(paste0("outputs/SGFCM_all_k6_entropy_", Sys.Date(), ".tif")))
-
 ## k = 8
-arch_all_rst <- rast(SGFCM_all_result_k8$rasters)
+arch_all_rst <- rast(SGFCM_result_k8$rasters)
 arch_all_rst_conus_belong <- subset(arch_all_rst, 1:8)
 
 arch_all_rst_conus_df <- as.data.frame(arch_all_rst, xy = TRUE)
@@ -67,7 +50,8 @@ arch_all_rst_conus_df <- as.data.frame(arch_all_rst, xy = TRUE)
 all_ent_conus <- calcUncertaintyIndex(as.matrix(as.data.frame(arch_all_rst_conus_belong)))
 
 all_ent_conus <- cbind(arch_all_rst_conus_df, all_ent_conus)
-all_ent_rst_conus <- rast(all_ent_conus)
+all_ent_rst_conus <- rast(all_ent_conus, crs = projection)
 
-writeRaster(all_ent_rst_conus, here::here(paste0("outputs/SGFCM_all_k8_entropy_", Sys.Date(), ".tif")))
+writeRaster(all_ent_rst_conus, here::here(paste0("outputs/models/SGFCM_k8_entropy_", Sys.Date(), ".tif")))
+
 
