73fe56c9bb08a6901b155fa520edd6390b63bdce
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-05-12 14:27:49 -0600

    use 2024 census, calculate the average net change in population from 2020-2024. Make new raster file for ave change in population

--- scripts/test/01_census_data_prep.R
+++ scripts/test/01_census_data_prep.R
@@ -26,8 +26,11 @@ if (file.exists(sub_dir)){
 }
 
 # Change in population (from Census)
-url <- "https://www2.census.gov/programs-surveys/popest/datasets/2020-2023/counties/totals/co-est2023-alldata.csv"
-file_name <- "co-est2023-alldata.csv"
+# from https://www.census.gov/data/tables/time-series/demo/popest/2020s-counties-total.html
+#url <- "https://www2.census.gov/programs-surveys/popest/datasets/2020-2023/counties/totals/co-est2023-alldata.csv"
+#url <- "https://www2.census.gov/programs-surveys/popest/tables/2020-2024/counties/totals/co-est2024-pop.xlsx"
+url <- "https://www2.census.gov/programs-surveys/popest/datasets/2020-2024/counties/totals/co-est2024-alldata.csv"
+file_name <- "co-est2024-alldata.csv"
 
 download.file(url, here::here(paste0("data/original/", sub_dir, "/", file_name)))
 
@@ -35,7 +38,7 @@ download.file(url, here::here(paste0("data/original/", sub_dir, "/", file_name))
 projection <- "epsg:5070"
 
 # Load the data
-del_pop_cen <- read_csv(here::here("data/original/census/census_2023_comp_est.csv"))
+del_pop_cen <- read_csv(here::here("data/original/census/co-est2024-alldata.csv"))
 
 # Download the county data for Idaho. Set the year to 2023
 counties_2023 <- tigris::counties(year = 2023) 
@@ -46,9 +49,9 @@ counties_2023 <- st_transform(counties_2023, projection)
 
 ## Net and ave change in population and % change in migration (rate?), using census data
 delpop_cen <- del_pop_cen %>% 
-  mutate(pct_mig = ((NPOPCHG2023 - NPOPCHG2020)/NPOPCHG2020) * 100,
-         ave_del_pop = ((NPOPCHG2020 + NPOPCHG2021 + NPOPCHG2022 + NPOPCHG2023)/4)) %>%
-  dplyr::select(STATE, COUNTY, NPOPCHG2023, NETMIG2022, ave_del_pop, pct_mig)
+  mutate(pct_mig_5yr = ((NPOPCHG2024 - NPOPCHG2020)/NPOPCHG2020) * 100,
+         ave_del_pop = ((NPOPCHG2020 + NPOPCHG2021 + NPOPCHG2022 + NPOPCHG2023 + NPOPCHG2024)/5)) %>%
+  dplyr::select(STATE, COUNTY, NPOPCHG2023, NETMIG2022, ave_del_pop, pct_mig_5yr)
 
 delpop_cen <- delpop_cen %>%
   mutate(STATE = as.character(STATE), 
@@ -64,7 +67,7 @@ update_fips <- function(data_set) {
 
 delpop_fips <- update_fips(delpop_cen)
 
-## join to the 2020 counties
+## join to the 2023 counties
 delpop_county <- left_join(counties_2023, delpop_fips,
                            by = c("GEOID" = "FIPS"))
 
@@ -103,14 +106,31 @@ nrow(as.data.frame(delpop.rst))
 # Fill NAs with focal
 delpop_fill <- focal(delpop.rst, 3, mean, na.policy='only', na.rm = TRUE)
 plot(delpop_fill)
-nrow(as.data.frame(delpop_crop_fill))
+nrow(as.data.frame(delpop_fill))
 
 # Crop to reference raster
 delpop_fill_crop <- crop(delpop_fill, ref_rast, mask = TRUE)
 plot(delpop_fill_crop)
 nrow(as.data.frame(delpop_fill_crop))
 
-# Save raster 
+# Rasterize the ave population change from 2020-2024 data using the templateRas as the reference raster
+avedelpop.rst <- rasterize(delpop_proj, ref_rast, field = "ave_del_pop", fun = "mean")
+nrow(as.data.frame(avedelpop.rst))
+
+# Fill NAs with focal
+avepop_fill <- focal(avedelpop.rst, 3, mean, na.policy='only', na.rm = TRUE)
+plot(avepop_fill)
+nrow(as.data.frame(avepop_fill))
+
+# Crop to reference raster
+avepop_fill_crop <- crop(avepop_fill, ref_rast, mask = TRUE)
+plot(avepop_fill_crop)
+nrow(as.data.frame(avepop_fill_crop))
+
+# Save rasters 
 writeRaster(delpop_fill_crop, here::here(paste0("data/processed/net_mig_2023_id_3km_crop_", 
                                 Sys.Date(), ".tif")))
+
+writeRaster(avepop_fill_crop, here::here(paste0("data/processed/ave_pop_change_2020_2024_id_3km_crop_", 
+                                                Sys.Date(), ".tif")))
 
