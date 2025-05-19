e4531d36c90ee9f1a5fa997d2bd832172cbe3374
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-05-19 16:06:41 -0600

    trying to figure out road density

--- scripts/test/01_roads_data_prep.R
+++ scripts/test/01_roads_data_prep.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(raster)
 library(sf)
 library(terra)
 library(tigris)
@@ -7,6 +8,7 @@ library(RCurl)
 library(stars)
 library(spdep)
 library(gstat)
+library(spatstat)
 
 # Set timeout to 10 minutes
 options(timeout=6000)
@@ -25,6 +27,9 @@ if (file.exists(sub_dir)){
   dir.create(file.path(data_dir, sub_dir))
 }
 
+# Set the projection
+projection <- "epsg:5070"
+
 # Road density (from TIGRIS)
 # Download the road data for Idaho. Set the year to 2023
 # could do all roads - would have to loop through counties 
@@ -33,7 +38,7 @@ roads_2023 <- tigris::primary_secondary_roads("Idaho", year = "2023")
 roads_proj <- st_transform(roads_2023, projection)
 
 gg <- ggplot()
-gg <- gg + geom_sf(data = roads_proj,
+gg <- gg + geom_sf(data = nw_roads,
                    color="black", fill="white", size=0.25)
 gg
 
@@ -43,7 +48,7 @@ rails_proj <- st_transform(rails_2023, projection)
 gg <- ggplot()
 gg <- gg + geom_sf(data = rails_2023,
                    color="black", fill="white", size=0.25)
-gg <- gg + theme_map()
+
 gg
 
 # Download the county data for Idaho. Set the year to 2023
@@ -53,10 +58,67 @@ counties_2023 <- counties_2023 %>%
   dplyr::select(GEOID, geometry)
 counties_2023 <- st_transform(counties_2023, projection)
 
-# I need where the rail and Idaho counties intersect
-id_rails <- st_intersection(rails_proj, counties_2023) 
+## get the state boundaries
+states <- tigris::states()
+
+## reproject
+states_proj <- st_transform(states, crs = projection)
+
+## get the idaho boundary
+id_bdry <- states_proj %>%
+  filter(STUSPS == "ID")
+
+## get the boundary for states neighboring Idaho 
+## we will use the NW states to calculate the distance to energy sources
+nw_states <- states_proj %>%
+  filter(STUSPS %in% c("ID", "WA", "MT", "OR", "NV", "UT", "WY", "CA"))
+
+# I need where the roads and NW states intersect
+nw_roads <- tigris::primary_secondary_roads(state = , year = "2023")
+#nw_roads <- st_intersection(roads_proj, nw_states) 
 
 gg <- ggplot()
-gg <- gg + geom_sf(data = id_rails,
+gg <- gg + geom_sf(data = nw_roads,
                    color="black", fill="white", size=0.25)
 gg
+
+# Create a template raster for the shapefiles
+XMIN <- ext(nw_states)$xmin
+XMAX <- ext(nw_states)$xmax
+YMIN <- ext(nw_states)$ymin
+YMAX <- ext(nw_states)$ymax
+aspectRatio <- (YMAX-YMIN)/(XMAX-XMIN)
+cellSize <- 3000
+NCOLS <- as.integer((XMAX-XMIN)/cellSize)
+NROWS <- as.integer(NCOLS * aspectRatio)
+templateRas <- rast(ncol=NCOLS, nrow=NROWS, 
+                    xmin=XMIN, xmax=XMAX, ymin=YMIN, ymax=YMAX,
+                    vals=1, crs=crs(nw_states))
+
+# Rasterize the roads shapefile
+nw_road_rast <- rasterize(vect(nw_roads$geometry), templateRas)
+
+# Calculate the distance from power plants
+nwrd_dist_rast <- terra::distance(nw_road_rast)
+
+
+plot(nwrd_dist_rast)
+
+# I need where the rail and Idaho counties intersect
+nw_rails <- st_intersection(rails_proj, nw_states) 
+
+gg <- ggplot()
+gg <- gg + geom_sf(data = nw_rails,
+                   color="black", fill="white", size=0.25)
+gg
+
+
+# Rasterize the roads shapefile
+nw_rail_rast <- rasterize(vect(nw_rails$geometry), templateRas)
+
+# Calculate the distance from rails
+nwrl_dist_rast <- terra::distance(nw_rail_rast)
+
+plot(nwrl_dist_rast)
+
+psp_rail <- as.psp(nw_roads)
